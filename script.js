e531757f73af0c9ff059633ccb3c15954aa9b5df
MrBig83 <mjohansson176@gmail.com>
2023-05-03 18:50:59 +0200

    Added Posts. Styling still TODO

--- script.js
+++ script.js
@@ -2,35 +2,59 @@
 const btn = document.querySelector ("button");
 const container2 = document.querySelector (".container2");
 const h1 = document.querySelector ("h1");
-
-
+const huvudbild = document.querySelector (".huvudbild");
+const h3container = document.querySelector (".h3container");
+const pListContainer = document.querySelector (".pListContainer");
+const dynamic = document.querySelector (".dynamic");
+const contentContainer = document.querySelector (".contentContainer");
 //Här ska vi ha fetch
+//=========================================================
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow', 
+    
+  };
 
-function getPosts() {
-    fetch("http://localhost/WP-Projektarbete-MEGA/wp-json/wp/v2/posts")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(posts) {
-        listOfPosts = posts;
-        addPostsToWebpage();
-    });   
+async function getProducts(){
+  const response = await fetch("http://localhost/WP-Projektarbete-MEGA/wp-json/wc/v3/products?token=kaffe")
+  const data = await response.json();
+  console.log(data);
+}
+  
+async function getPosts() {
+    const response = await fetch("http://localhost/WP-Projektarbete-MEGA/wp-json/wp/v2/posts")
+    const data = await response.json();
+    console.log(data);
+    addPostsToWebpage(data);
+}
+getPosts();
+getProducts();
+
+function addPostsToWebpage(data) {
+
+  const div = document.createElement("div");
+  div.classList.add("contentDiv")
+  contentContainer.appendChild(div);
+
+  for (const post of data) {
+  
+    const div = document.createElement("div");
+    div.classList.add("contentWrapper")
+    contentContainer.appendChild(div)
+
+    const img = document.createElement("img");
+    img.classList.add("css-for-image");
+    div.appendChild(img);
+    img.src = post.images.medium 
+
+    const p = document.createElement("p");
+    p.classList.add("css-for-postname");
+    p.innerText = post.title.rendered;
+    div.appendChild(p);
+    
+  }
 }
 
-function addPostsToWebpage() {
-    const dynamicContainer = document.querySelector(".dynamic");
-    for (const post of listOfPosts) {
-
-        const img = document.createElement("img");
-        img.classList.add("css-for-image");
-        dynamicContainer.appendChild(img);
-        img.src = `${post.featured_media}`   //Detta måste kopplas till json respons?
-
-        const h3 = document.createElement("h3");
-        h3.classList.add("css-for-postname");
-        h3.innerText = post.name;
-        div.appendChild(h3);
-
-       };
-    }
-
+function addProductsToWebpage(data){
+  console.log(data)
+}
